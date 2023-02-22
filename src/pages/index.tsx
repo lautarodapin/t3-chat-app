1cf1ccae2e8ff47f6bd1dca977a7e9931d312751
lautaro-magoya <lautaro@magoya.com>
2023-02-22 18:52:40 -0300

    Cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import {type NextPage} from "next"
 import {ChatPage} from 'src/components/layout/chat-page'
 
-import {Sidebar} from '../components/sidebar'
-
 const Home: NextPage = () => {
     return (
         <ChatPage />
