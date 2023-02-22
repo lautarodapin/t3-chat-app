be7cc6db3870daea07bd0c0cb488b57ba25cdbbd
lautaro-magoya <lautaro@magoya.com>
2023-02-22 18:49:07 -0300

    Add reducer to input

--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -7,7 +7,23 @@ type Props = {
     chatId: Chat['id']
     onMessageCreated?: (data: RouterOutputs['chat']['sendMessage']) => void
 }
+
+type ReducerState = {
+    input: string
+    error: null | string
+}
+type ReducerAction = string
+
 export const ChatInput: React.FC<Props> = ({chatId, onMessageCreated}) => {
+    const [state, dispatch] = React.useReducer((state: ReducerState, newInput: ReducerAction) => {
+        return {
+            ...state,
+            input: newInput,
+            error: !newInput && 'Debes escribir algo'
+                || newInput.length < 3 && 'Debe tener mas de 3 caracteres'
+                || null,
+        }
+    }, {input: '', error: null})
     const [input, setInput] = React.useState('')
     const {mutateAsync} = api.chat.sendMessage.useMutation({})
     return (
@@ -15,20 +31,23 @@ export const ChatInput: React.FC<Props> = ({chatId, onMessageCreated}) => {
             className='flex space-x-2 w-full'
             onSubmit={async e => {
                 e.preventDefault()
-                if (!input || input.length < 3) return
+                if (state.error !== null) return
                 const newMsg = await mutateAsync({chatId, content: input})
                 onMessageCreated?.(newMsg)
                 setInput('')
             }}>
-            <input
-                className={clsx('input flex-1 input-primary')}
-                value={input}
-                placeholder='Escribi un mensaje...'
-                onChange={e => setInput(e.target.value)}
-            />
+            <div className="form-control w-full">
+                <input
+                    className={clsx('input flex-1 input-primary', {'input-error': state.error !== null})}
+                    value={state.input}
+                    placeholder='Escribi un mensaje...'
+                    onChange={e => dispatch(e.target.value)}
+                />
+            </div>
+
             <button
                 className={clsx('btn btn-primary')}
-                type='submit'>send</button>
+                type='submit'>Enviar</button>
         </form>
     )
 
